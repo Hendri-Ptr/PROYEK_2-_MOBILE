1de562a2a9470cb1311b8a19d3a2e8f00956a77d
hendri-ptr <teamsgrhen@gmail.com>
2025-04-25 08:28:29 +0700

    Update

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -4,7 +4,7 @@ import 'settings_screen.dart';
 import 'paket_screen.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
+  const MainScreen({super.key});
 
   @override
   State<MainScreen> createState() => _MainScreenState();
